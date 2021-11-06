e922a3eee54e681c546a3553d371914e15c76b5a
Ilir Perolli <ilir_perolli@live.com>
2021-11-06 18:49:45 +0100

    Being able to multiply

---
+++ src/Multiplication.java
@@ -0,0 +1,118 @@
+public class Multiplication {
+    public static String multiply(String s1, String s2){
+
+        String number1 = s1;
+        String number2 = s2;
+
+        number1 = reverse(number1);
+        number2 = reverse(number2);
+
+        String [] multiply = new String[number2.length()];
+        String mult = "";
+        int reminder = 0;
+
+        for (int i = 0; i < number2.length(); i++) {
+
+            mult = "";
+
+            for (int j = 0; j < number1.length(); j++) {
+
+                int multipliedNumbers = (Integer.parseInt(Character.toString(number2.charAt(i))) * Integer.parseInt(Character.toString(number1.charAt(j))))+reminder;
+                if (multipliedNumbers > 9 && j != number1.length()-1){
+
+                    reminder = multipliedNumbers / 10;
+                    multipliedNumbers = multipliedNumbers % 10;
+
+                }
+                else reminder = 0;
+
+                mult = multipliedNumbers + mult;
+
+            }
+
+            multiply[i] = mult;
+
+        }
+
+        // shto 0 ne vargun multiply djatht
+        String zeros = "";
+
+        for (int i=0; i<multiply.length; i++){
+            zeros = "";
+            if (i != 0){
+                for (int j=0; j<i;j++){
+                   zeros += "0";
+                }
+                multiply[i] = multiply[i]+zeros;
+            }
+        }
+        // shto 0 ne vargun multiply majt
+        for (int i=0; i<multiply.length; i++){
+            zeros = "";
+            int loop = maxValue(multiply) - multiply[i].length();
+            for (int j = 0; j < loop; j++){
+                zeros += "0";
+            }
+            multiply[i] = zeros + multiply[i];
+
+        }
+
+        String multiplyAcc = multiply[0];
+        for (int i=1; i<multiply.length; i++){
+            multiplyAcc = sumNumbers(reverse(multiplyAcc), reverse(multiply[i]));
+        }
+
+        for (int i=0; i<multiply.length; i++){
+            System.out.println(multiply[i]);
+        }
+
+        return multiplyAcc;
+    }
+
+    public static int maxValue(String [] arr){
+        int max = 0;
+        for (int i = 0; i<arr.length; i++){
+            if (arr[i].length() > arr[max].length()){
+                max = i;
+            }
+        }
+        return arr[max].length();
+    }
+    public static String reverse(String s1){
+        char r;
+        String reversedSum = "";
+        for (int i=0; i<s1.length(); i++)
+        {
+            r= s1.charAt(i);
+            reversedSum= r+reversedSum;
+        }
+        return reversedSum;
+    }
+
+    public static String sumNumbers(String number1, String number2){
+        int reminder = 0;
+
+        String sum = "";
+        for (int i = 0; i < number1.length(); i++) {
+            int addedNumbers = (Integer.parseInt(Character.toString(number1.charAt(i))) + Integer.parseInt(Character.toString(number2.charAt(i))))+reminder;
+            if (addedNumbers > 9){
+                addedNumbers = addedNumbers % 10;
+                reminder = 1;
+            }
+            else reminder = 0;
+
+            sum += addedNumbers;
+
+            if (i == number1.length()-1 && reminder == 1){
+                sum += "1";
+            }
+        }
+        return reverse(sum);
+    }
+    public static void main(String[] args){
+        String s1 = "1234564";
+        String s2 = "21564651";
+        System.out.println(s1 + " * " + s2 + " = " + multiply(s1,s2));
+
+    }
+}
--- src/Summation.java
+++ src/Summation.java
@@ -55,8 +55,8 @@ public class Summation {
         return reverse;
     }
     public static void main(String[] args){
-        String s1 = "250505050202020505051020202022100";
-        String s2 = "062051505050505050505050585055050055052";
+        String s1 = "063120";
+        String s2 = "007101";
         System.out.print(s1 + " + " + s2 + " = " + sum(s1,s2));
     }
 }
